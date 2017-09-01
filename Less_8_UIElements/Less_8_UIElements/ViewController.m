799fdfe3bf2ed12acee381dd2fa6d0d8d67f1ff0
Andrey Proskurin <andrewproskurin@gmail.com>
2017-09-01 21:46:47 +0300

    Lesson_8 .Использование textField и textView

--- Less_8_UIElements/Less_8_UIElements/ViewController.m
+++ Less_8_UIElements/Less_8_UIElements/ViewController.m
@@ -8,7 +8,17 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+typedef struct {
+    
+    CGFloat weight;
+    NSUInteger age;
+    
+} Person;
+
+@interface ViewController () <UITextFieldDelegate, UITextViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
 
 @end
 
@@ -16,13 +26,84 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    
+    NSString *someString = @"I will be iOS developer";
+    
+    NSRange range = NSMakeRange(2, 4);
+    NSString *resultString = [someString substringWithRange:range];
+    NSLog(@"%@", resultString);
+    
+    Person person;
+    person.age = 20;
+    person.weight = 76;
+    
+    if (textField.tag == 5) {
+        
+        CGRect frame = textField.frame;
+        
+        frame.origin.y = self.emailTextField.frame.origin.y + self.emailTextField.frame.size.height + 40;
+        
+        textField.frame = frame;
+    }
+    
+    
+    
+    NSLog(@"\ntextFieldDidBeginEditing\n");
+    if (textField.tag == 5) {
+        textField.secureTextEntry = YES;
+    }
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    NSLog(@"\ntextFieldDidEndEditing\n");
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    // автоматический переход между textField-ами
+    if (textField.tag == 5) {
+        [self.emailTextField becomeFirstResponder];
+    } else {
+        [self.passwordTextField becomeFirstResponder];
+    }
+    NSLog(@"\ntextFieldShouldReturn\n");
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField {
+    if (textField.text.length < 5) {
+        return NO;
+    }
+    NSLog(@"\ntextFieldShouldEndEditing\n");
+    return YES;
+}
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    if (textField.tag == 5) {
+        if (self.emailTextField.text.length < 5) {
+            return NO;
+        }
+    }
+    NSLog(@"\ntextFieldShouldBeginEditing\n");
+    return YES;
+}
+
+// основной метод (вызывается во время редактирования [во время изменения текстового поля])
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    
+    NSLog(@"\nshouldChangeCharactersInRange\n");
+    
+    if ([string isEqualToString:@"+"]) {
+        return NO;
+    }
+    return YES;
+}
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    return YES;
 }
 
 
